060e9febb984ba8ae3e4804367ae7f48cb07dac5
luisholman <88738669+luisholman@users.noreply.github.com>
2021-09-15 17:58:39 -0500

    Create registrar.php

---
+++ Tarifas/registrar.php
@@ -0,0 +1,22 @@
+<?php
+$mysqli= include_once "../Database/conexion.php";
+$Tarifa_minuto= $_POST["minuto"];
+$Tarifa_hora= $_POST["hora"];
+$Tarifa_dia= $_POST["dia"];
+$Tarifa_noche= $_POST["noche"];
+$Tarifa_semana= $_POST["semana"];
+$Tarifa_mes= $_POST["mes"];
+
+$queryInsercion=$mysqli->prepare("INSERT INTO tarifas
+(Tarifa_minuto,
+Tarifa_hora,
+Tarifa_dia,
+Tarifa_noche,
+Tarifa_semana,
+Tarifa_mes)
+VALUES
+(?,?,?,?,?,?)");
+$queryInsercion->bind_param("iiiiii",$Tarifa_minuto,$Tarifa_hora,$Tarifa_dia,$Tarifa_noche,$Tarifa_semana,$Tarifa_mes);
+$queryInsercion->execute();
+header("location: listar.php");
+?>
