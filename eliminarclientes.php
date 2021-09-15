f30799e14ebc9ae6ec5b35f5f6ccc467b6bea1ec
luisholman <88738669+luisholman@users.noreply.github.com>
2021-09-15 17:55:40 -0500

    Create eliminarclientes.php

---
+++ eliminarclientes.php
@@ -0,0 +1,15 @@
+<?php
+$mysqli= include_once "./database/conexion.php";
+if (!isset($_GET["id"])) {
+    exit ("no se tiene un id");
+}else {
+    $Id_cliente= $_GET["id"];
+$queryEliminacion=$mysqli->prepare("DELETE FROM clientes
+WHERE 
+Id_cliente=?");
+$queryEliminacion->bind_param("i",$Id_cliente);
+$queryEliminacion->execute();
+header("location: listarclientes.php");
+}
+
+?>
