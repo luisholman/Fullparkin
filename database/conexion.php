4965dc663bf37841abc321fee5299e04d5722d17
luisholman <88738669+luisholman@users.noreply.github.com>
2021-09-15 17:55:02 -0500

    Create conexion.php

---
+++ database/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$host="localhost";
+$usuario="root";
+$contra="";
+$base_de_datos="Parkingfull";
+
+$mysqli= new mysqli($host,$usuario,$contra,$base_de_datos);
+if($mysqli->connect_errno){
+    echo"ha ocurrido un error al conectarse a la bd".$mysqli->connect_errno;
+}else{
+   // echo "se ha conectado satisfactoriamente";
+}
+return $mysqli;
+
+?>
