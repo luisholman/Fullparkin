43780d7e5020a71f58f47ce26f463b1b4664119b
luisholman <88738669+luisholman@users.noreply.github.com>
2021-09-21 08:53:12 -0500

    Create actualizar_Admin.php

---
+++ actualizar_Admin.php
@@ -0,0 +1,23 @@
+<?php
+    $mysqli= include_once "./database/conexion.php";
+
+    $Administrador= $_POST["administrador"];
+    $Empleado= $_POST["empleado"];
+    $Password= $_POST["password"];
+    $Email= $_POST["email"];
+    $idAdmin= $_POST["id"];
+    $queryActualizacion=$mysqli->prepare("UPDATE admin
+    SET
+    Administrador=?,
+    Empleado=?,
+    Password=?,
+    Email=?                          
+    WHERE idAdmin= ?");
+
+    $queryActualizacion->bind_param("ssssi",$Administrador,$Empleado,$Password,$Email,$idAdmin);
+    $queryActualizacion->execute();
+    header("location: listar_Admin.php");
+?>
+
+
+
