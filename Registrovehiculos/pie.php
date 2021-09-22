480032757d43e191c495de70a54f143128435c90
luisholman <88738669+luisholman@users.noreply.github.com>
2021-09-21 20:50:35 -0500

    Delete pie.php

--- Registrovehiculos/pie.php
+++
@@ -1,8 +0,0 @@
-</main>
-<footer class="px-2 py-2 fixed-bottom bg -dark">
-<span class="text-muted">CRUD DE MYSQLI CON PHP CREADO POR LUIS HOLMAN OICATA JAIME</span>
-
-
-</footer>
-</body>
-</html>
