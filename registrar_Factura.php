9568ecd15d7e45def9c2af79f3b69f3cee49edba
luisholman <88738669+luisholman@users.noreply.github.com>
2021-09-21 20:50:00 -0500

    Create registrar_Factura.php

---
+++ registrar_Factura.php
@@ -0,0 +1,25 @@
+<?php
+$mysqli= include_once "./database/conexion.php";
+$Placa= $_POST["placa"];
+$Fecha_de_Ingreso= $_POST["fechaingreso"];
+$Hora_Ingreso= $_POST["horaingreso"];
+$Fecha_de_salida= $_POST["fechasalida"];
+$Hora_de_salida= $_POST["horasalida"];
+$Subtotal= $_POST["subtotal"];
+$Impuestos= $_POST["impuestos"];
+$Total= $_POST["total"];
+$queryInsercion=$mysqli->prepare("INSERT INTO factura
+(Placa,
+Fecha_de_Ingreso,
+Hora_Ingreso,
+Fecha_de_salida,
+Hora_de_salida,
+Subtotal,
+Impuestos,
+Total)
+VALUES
+(?,?,?,?,?,?,?,?)");
+$queryInsercion->bind_param("sddddiii",$Placa,$Fecha_de_Ingreso,$Hora_Ingreso,$Fecha_de_salida,$Hora_de_salida,$Subtotal,$Impuestos,$Total);
+$queryInsercion->execute();
+header("location: listar_Factura.php");
+?>
