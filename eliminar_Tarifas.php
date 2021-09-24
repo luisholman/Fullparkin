b93c5b95176a0da71fc9485d6b0057bf0c89e1cd
luisholman <88738669+luisholman@users.noreply.github.com>
2021-09-23 20:01:05 -0500

    Create eliminar_Tarifas.php

---
+++ eliminar_Tarifas.php
@@ -0,0 +1,15 @@
+<?php
+$mysqli= include_once "./Database/conexion.php";
+if (!isset($_GET["id"])) {
+    exit ("no se tiene un id");
+}else {
+    $idTarifas= $_GET["id"];
+$queryEliminacion=$mysqli->prepare("DELETE FROM tarifas
+WHERE 
+idTarifas=?");
+$queryEliminacion->bind_param("i",$idTarifas);
+$queryEliminacion->execute();
+header("location: listar_Tarifas.php");
+}
+
+?>
