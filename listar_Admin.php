9e5f3f17653e7dd884c8e1deb9a50a70ab16767d
luisholman <88738669+luisholman@users.noreply.github.com>
2021-10-06 14:29:33 -0500

    Update listar_Admin.php

--- listar_Admin.php
+++ listar_Admin.php
@@ -43,10 +43,10 @@ $listadoClientes= $resultadoQuery->fetch_all(MYSQLI_ASSOC);
 
 
            <td>
-               <a href="editar_Admin.php?id=<?php echo $listadoIndividual["idAdmin"] ?>">Editar</a>
+               <a class="btn btn-primary" href="editar_Admin.php?id=<?php echo $listadoIndividual["idAdmin"] ?>">Editar</a>
            </td>
            <td>
-               <a href="eliminar_Admin.php?id=<?php echo $listadoIndividual["idAdmin"] ?>">Eliminar</a>
+               <a class="btn btn-secondary" href="eliminar_Admin.php?id=<?php echo $listadoIndividual["idAdmin"] ?>">Eliminar</a>
            </td>
        </tr>
     <?php
