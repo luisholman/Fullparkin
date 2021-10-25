bc3915f45709a6e457db6640259890b9b865c467
luisholman <88738669+luisholman@users.noreply.github.com>
2021-10-25 13:00:27 -0500

    Update pie.php

--- vistas/pie.php
+++ vistas/pie.php
@@ -31,8 +31,8 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js" ></script>
 
-<script type="text/javascript" src="script.js" ></script>
-<script type="text/javascript" src="scriptnombre.js" ></script>
+<script type="text/javascript" src="script_placa_registro_vehiculos.js" ></script>
+<script type="text/javascript" src="script_nombre_registro_clientes.js" ></script>
 <script type="text/javascript" src="script_nombre_registro_vehiculos.js" ></script>
 
 </body>
