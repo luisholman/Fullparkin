c05aa1ecfba43a87061fe29ca57be07833290a97
luisholman <88738669+luisholman@users.noreply.github.com>
2021-10-25 12:44:59 -0500

    Create retornoData_nombre_registro_vehiculos.php

---
+++ retornoData_nombre_registro_vehiculos.php
@@ -0,0 +1,10 @@
+<?php
+$mysqli=include_once"conexion.php";
+$query=$_GET['query'];
+$resultado=mysqli_query($mysqli, "SELECT Nombre_cliente FROM registro_vehiculos WHERE Nombre_cliente LIKE '%".$query."%' ");
+$json=array();
+while ($rows = mysqli_fetch_assoc($resultado)){
+    $json[]=$rows["Nombre_cliente"];
+}
+echo json_encode($json);
+?>
