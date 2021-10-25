1bb97caf08b026c3572d69507b6b2f921a212203
luisholman <88738669+luisholman@users.noreply.github.com>
2021-10-25 12:44:48 -0500

    Update conexion.php

--- database/conexion.php
+++ database/conexion.php
@@ -6,7 +6,7 @@ $base_de_datos="Parkingfull";
 
 $mysqli= new mysqli($host,$usuario,$contra,$base_de_datos);
 if($mysqli->connect_errno){
-    echo"ha ocurrido un error al conectarse a la bd".$mysqli->connect_errno;
+    echo"ha ocurrido un error al conectarse a la bd".$mysqli->connect_error;
 }else{
    // echo "se ha conectado satisfactoriamente";
 }
