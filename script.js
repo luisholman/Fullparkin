70079b15caa0f317379f80051e22d85e311cbf3b
luisholman <88738669+luisholman@users.noreply.github.com>
2021-10-25 12:45:06 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('input.typeahead').typeahead({
+        source: function(query,process){
+            return $.get('retornoData.php', { query:query },function(data){
+                data= $.parseJSON(data);
+                return process(data);
+            });
+
+        },
+        showHintOnFocus:'all'
+    });
+});
