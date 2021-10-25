dde51c838769f2c79cc576fe2540d5d5e45a0eac
luisholman <88738669+luisholman@users.noreply.github.com>
2021-10-25 13:00:02 -0500

    Update retornoData_nombre_registro_vehiculos.php

--- retornoData_nombre_registro_vehiculos.php
+++ retornoData_nombre_registro_vehiculos.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli=include_once"conexion.php";
+$mysqli=include_once"./database/conexion.php";
 $query=$_GET['query'];
 $resultado=mysqli_query($mysqli, "SELECT Nombre_cliente FROM registro_vehiculos WHERE Nombre_cliente LIKE '%".$query."%' ");
 $json=array();
